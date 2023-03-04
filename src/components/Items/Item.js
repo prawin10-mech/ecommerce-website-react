0b8de196f58e7192b5a92d12e1159d52aa4509a5
prawin10-mech <pra6650@gmail.com>
2023-03-04 11:20:36 +0530

    added fetched data

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import AvailableItems from "./AvailableItems";
 import classes from "./Items.module.css";
+import { Button } from "react-bootstrap";
 
 const Item = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchData = async () => {
+    setIsLoading(true);
+    let response = await fetch("https://swapi.dev/api/films");
+    response = response.json();
+    response = response.results;
+    console.log(response);
+    setIsLoading(false);
+  };
   return (
-    <div className={classes.items}>
-      <AvailableItems />
-    </div>
+    <>
+      <Button onClick={fetchData}>Fetch data</Button>
+      {!isLoading && (
+        <div className={classes.items}>
+          <AvailableItems />
+        </div>
+      )}
+      {isLoading && <p>loading.....</p>}
+    </>
   );
 };
 
